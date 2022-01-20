31c07195b42ecad7b7ad08e4c7fe39e41e2fc617
LBS IT <percylee@lbsgroup.com.hk>
2022-01-20 12:17:19 +0800

    物料/快捷语/风险害虫 汇出表格时 增加显示地区

--- protected/models/RptRiskpestlist.php
+++ protected/models/RptRiskpestlist.php
@@ -3,9 +3,13 @@ class RptRiskpestlist extends CReport {
 	public $show_report_title = false;
 	
 	protected function fields() {
-		return array(
+		$part1 = array(
+            'city_name'=>array('label'=>Yii::t('material','City_name'),'width'=>10,'align'=>'C'),
+		);
+		$part2 = array(
             'target'=>array('label'=>Yii::t('risk','Target'),'width'=>50,'align'=>'L'),
 		);
+		return Yii::app()->user->isSingleCity() ? $part2 : array_merge($part1, $part2);
 	}
 
 	public function genReport() {
