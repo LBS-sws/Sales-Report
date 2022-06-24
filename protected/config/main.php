7a50beab5ed9c8b77bf49075408a6603375bd6f5
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2022-06-24 15:12:27 +0800

    正式版服务报告--“物料使用害虫”字眼，需修改为“物料使用靶标。PDF服务报告里的“点评时间”删除

--- protected/config/main.php
+++ protected/config/main.php
@@ -118,6 +118,6 @@ return array(
         'appname'=>'LBS DMS (UAT)',
         'appcolor'=>'skin-purple-light',
         'showRank'=>'on',
-        'baseUrl_imgs'=>'https://operation.lbsapps.cn/',// 正式版：https://xcx.lbsapps.cn/ 测试版：https://operation.lbsapps.cn/
+        'baseUrl_imgs'=>'https://xcx.lbsapps.cn/',// 正式版：https://xcx.lbsapps.cn/ 测试版：https://operation.lbsapps.cn/
     ),
 );
