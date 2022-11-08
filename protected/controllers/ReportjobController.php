8cd7510ef96eb36ac0249c09169429b3da9c2183
xiangsong <xiangsong.cd@lbsgroup.com.cn>
2022-11-08 15:06:40 +0800

    debug--改为单引号。

--- protected/controllers/ReportjobController.php
+++ protected/controllers/ReportjobController.php
@@ -254,7 +254,7 @@ EOF;
 	public function actionDownloadzip($fid,$fileName='') {
 		$zipname = sys_get_temp_dir().'/'.$fid.'.zip';
 		header('Content-Type: application/zip');
-		header("Content-disposition: attachment; filename=服务报告.zip");
+		header('Content-disposition: attachment; filename='.$fileName.'.zip');
 		header('Content-Length: ' . filesize($zipname));
 		readfile($zipname);
 		unlink($zipname);
