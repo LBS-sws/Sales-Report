67a6d3099ca51638fb80cd924e6fcbdd00421ad0
6058841891 <yiming.cd@lbsgroup.com.cn>
2023-05-19 15:03:32 +0800

    更新发票已签收

--- protected/views/reportjob/_listdtl.php
+++ protected/views/reportjob/_listdtl.php
@@ -32,10 +32,9 @@
 		<a style="display: none;">0</a>
         <?php
         if($this->record['Pics']) {
-//            echo '<a href="'.Yii::app()->createUrl('reportjob/look',array('id'=>$this->record['JobID'])).'" target="_blank" style="font-size: 14px; color: #fff; background:#8e98a2; padding: 5px 7.5px; border-radius: 5px; ">发票签收</a>';
 
             $dlnk = Yii::app()->createUrl('reportjob/look', array('index' => $this->record['JobID']));
-            echo TbHtml::Button('<span class="fa fa-yc"></span> ' . "发票签收", array('submit' => $dlnk, 'size' => TbHtml::BUTTON_SIZE_SMALL));
+            echo TbHtml::Button('<span class="fa fa-yc"></span> ' . "发票已签收", array('submit' => $dlnk, 'size' => TbHtml::BUTTON_SIZE_SMALL));
         }
         ?>
     </td>
