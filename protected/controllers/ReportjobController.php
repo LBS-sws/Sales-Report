a5c896d506fd1c9c459e847d6e3b1fcc88794b51
605884189 <605884189@qq.com>
2023-06-15 22:57:36 +0800

    1、获取发票id

--- protected/controllers/ReportjobController.php
+++ protected/controllers/ReportjobController.php
@@ -45,7 +45,12 @@ class ReportjobController extends Controller
 	// 查看发票
 	public function actionLook(){
 		$id = isset($_GET['index']);
+		echo $id;
 		$item = Yii::app()->db->createCommand('SELECT pics FROM lbs_invoice WHERE jobid="'.$id.'" ')->queryRow();
+		
+		// $sql = "select code,code_old from hr$suffix.hr_employee where id=$id";
+		// $row = Yii::app()->db->createCommand($sql)->queryRow();
+		
 		print_r($item);
 		$pics = $item['pics'];
 		if($pics){
