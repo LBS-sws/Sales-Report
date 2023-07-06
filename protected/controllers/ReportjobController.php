23ee0828364648570b02dbe5b57c7f18c84fe41d
xiangsong <xiangsong.cd@lbsgroup.com.cn>
2023-07-06 12:53:14 +0800

    _blank

--- protected/controllers/ReportjobController.php
+++ protected/controllers/ReportjobController.php
@@ -57,18 +57,17 @@ class ReportjobController extends Controller
 
 
     public function actionChecklog(){
-            $job_id = $_GET['index']?$_GET['index']:'';
+            $job_id = $_GET['index'] ? $_GET['index'] : '';
         include_once Yii::app()->basePath . '/common/Utils.php';//引入类文件
         $utils = new Utils();
 //        $params_str = http_build_query($params);
         $res = $utils->httpCurl($utils->sign_url.'/index.php/api/CheckLog/index?job_id='.$job_id);
         $res_de = json_decode($res, true);
         if (isset($res_de) && $res_de['code'] == 0) {
-            Dialog::message(Yii::t('dialog','success'), Yii::t('dialog','下载中···'));
-            echo '<script>window.open("' . $res_de['data'] . '");</script>';
-//            $this->redirect(Yii::app()->createUrl('reportjob/index'));
+            echo '<script>window.open("' . $res_de['data'] . '", "_blank");</script>';
+            $this->redirect(Yii::app()->createUrl('reportjob/index'));
         }else{
-            Dialog::message(Yii::t('dialog','error'), Yii::t('dialog',$res_de['msg']));
+            Dialog::message(Yii::t('dialog','Information'), Yii::t('dialog',$res_de['msg']));
         }
 
     }
